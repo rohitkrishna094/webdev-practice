d231b07b361249107a6bca0001579baadbb68126
rohitkrishna094 <rohitkrishna094@gmail.com>
2018-06-25 06:03:05 -0500

    Added linkGenerator.js to update links in readme file

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Webdev-Practice
+# [Webdev-Practice](https://rohitkrishna094.github.io/Webdev-Practice/)
+
+Click the links below for their demonstration
+
+* [1_Weight-Converter](https://rohitkrishna094.github.io/Webdev-Practice/1_Weight-Converter/)
+* [2_Temperature-Converter](https://rohitkrishna094.github.io/Webdev-Practice/2_Temperature-Converter/)
+* [3_Shooter-Game](https://rohitkrishna094.github.io/Webdev-Practice/3_Shooter-Game/)
+* [4_Twitter-Bot](https://rohitkrishna094.github.io/Webdev-Practice/4_Twitter-Bot/)
+* [faq-app](https://rohitkrishna094.github.io/Webdev-Practice/faq-app/)
+* [TestProject](https://rohitkrishna094.github.io/Webdev-Practice/TestProject/)
---
+++ linkGenerator.js
@@ -0,0 +1,24 @@
+let fs = require('fs');
+let path = require('path');
+
+const isDirectory = source => fs.lstatSync(source).isDirectory();
+const getDirectories = source => fs.readdirSync(source).map(name => path.join(source, name)).filter(isDirectory);
+
+// Change these 
+let currentDir = "Webdev-Practice";
+let heading = "Webdev-Practice";
+
+let readmeFile = __dirname + '/README.md';
+
+let dirs = getDirectories("./");
+let links = [];
+for (let i = 1; i < dirs.length; i++) {
+    links.push(`https://rohitkrishna094.github.io/${currentDir}/${dirs[i]}/`);
+}
+
+let content = `# [${heading}](https://rohitkrishna094.github.io/${currentDir}/)\n\n`;
+content += "Click the links below for their demonstration\n\n";
+for (let i = 0; i < links.length; i++) {
+    content += `* [${dirs[i+1]}](${links[i]})\n`;
+}
+fs.writeFileSync(readmeFile, content);
