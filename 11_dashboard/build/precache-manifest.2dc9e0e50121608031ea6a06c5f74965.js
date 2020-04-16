aca650b571d5873091ac9dc962979278757bdcf6
rohitkrishna094 <rohitkrishna094@gmail.com>
2020-04-15 20:39:13 -0400

    updated stuff

---
+++ 11_dashboard/build/precache-manifest.2dc9e0e50121608031ea6a06c5f74965.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b5c9481b3c4fd5a3a4df4484d905f82",
+    "url": "/webdev-practice/11_dashboard/index.html"
+  },
+  {
+    "revision": "fa6e1c14c2e85e232f8f",
+    "url": "/webdev-practice/11_dashboard/static/css/main.305817c9.chunk.css"
+  },
+  {
+    "revision": "2e07164cd325e3e98529",
+    "url": "/webdev-practice/11_dashboard/static/js/2.4f0628a2.chunk.js"
+  },
+  {
+    "revision": "fa6e1c14c2e85e232f8f",
+    "url": "/webdev-practice/11_dashboard/static/js/main.8559c056.chunk.js"
+  },
+  {
+    "revision": "5c6dd25a4841bf001b2f",
+    "url": "/webdev-practice/11_dashboard/static/js/runtime~main.4bef10e3.js"
+  },
+  {
+    "revision": "c1b1e2d67ceffa9372a4b8b0af252ded",
+    "url": "/webdev-practice/11_dashboard/static/media/logo.c1b1e2d6.jpg"
+  }
+]);
--- linkGenerator.js
+++ linkGenerator.js
@@ -1,27 +1,34 @@
-let fs = require('fs');
-let path = require('path');
+let fs = require("fs");
+let path = require("path");
 
-const isDirectory = source => fs.lstatSync(source).isDirectory();
-const getDirectories = source =>
+const isDirectory = (source) => fs.lstatSync(source).isDirectory();
+const getDirectories = (source) =>
   fs
     .readdirSync(source)
-    .map(name => path.join(source, name))
+    .map((name) => path.join(source, name))
     .filter(isDirectory);
 
 // Change these
-let currentDir = 'webdev-practice';
-let heading = 'webdev-practice';
+let currentDir = "webdev-practice";
+let heading = "webdev-practice";
+let customSubUrls = {
+  "faq-app": "dist",
+  "11_dashboard": "build",
+};
 
-let readmeFile = __dirname + '/README.md';
+let readmeFile = __dirname + "/README.md";
 
-let dirs = getDirectories('./');
+let dirs = getDirectories("./");
 let links = [];
 for (let i = 1; i < dirs.length; i++) {
-  links.push(`https://rohitkrishna094.github.io/${currentDir}/${dirs[i]}/`);
+  const dir = dirs[i];
+  let subUrl = "";
+  if (dir in customSubUrls) subUrl = customSubUrls[dir];
+  links.push(`https://rohitkrishna094.github.io/${currentDir}/${dir}/${subUrl}`);
 }
-
+console.log(links);
 let content = `# [${heading}](https://rohitkrishna094.github.io/${currentDir}/)\n\n`;
-content += 'Click the links below for their demonstration\n\n';
+content += "Click the links below for their demonstration\n\n";
 for (let i = 0; i < links.length; i++) {
   content += `* [${dirs[i + 1]}](${links[i]})\n`;
 }
