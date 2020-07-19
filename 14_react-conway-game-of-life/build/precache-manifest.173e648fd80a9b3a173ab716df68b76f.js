ced741e7e47c9199232f081dc2c69e5fb81c7701
rohitkrishna094 <rohitkrishna094@gmail.com>
2020-07-18 23:49:04 -0400

    build for conway

--- 14_react-conway-game-of-life/build/precache-manifest.173e648fd80a9b3a173ab716df68b76f.js
+++ 14_react-conway-game-of-life/build/precache-manifest.173e648fd80a9b3a173ab716df68b76f.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f008cd8f75bcf209ddbb242e0a96ebb2",
+    "revision": "0eb62946ff1e4c84096356cc5d1194ca",
     "url": "./index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/css/2.8af6af7f.chunk.css"
   },
   {
-    "revision": "974e25e109eba385ffcb",
-    "url": "./static/css/main.76da25e8.chunk.css"
+    "revision": "89ed1216063d3a6586da",
+    "url": "./static/css/main.c6c391e9.chunk.css"
   },
   {
     "revision": "0c961fd0bb6c1eadc6ff",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.ac539b4a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "974e25e109eba385ffcb",
-    "url": "./static/js/main.5ca8b93b.chunk.js"
+    "revision": "89ed1216063d3a6586da",
+    "url": "./static/js/main.093f3a9a.chunk.js"
   },
   {
     "revision": "b83506ab5c7901605ea6",
